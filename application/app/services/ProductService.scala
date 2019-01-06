dcec3c25fed60bfa513164a09afb3cc9c559dcdc
quang_vm <quang_vm@septeni-technology.jp>
2019-01-06 17:20:46 +0700

    + add response template to product

--- application/app/services/ProductService.scala
+++ application/app/services/ProductService.scala
@@ -1,24 +1,32 @@
 package services
 
 import javax.inject.Inject
-import models.ProductRepository
-import models.Product
-import play.api.libs.json.{ JsNumber, JsObject, JsString }
+import models.{Product, ProductRepository, ProductTemplateRepository, ProductTemplate}
+import play.api.libs.json.{JsNumber, JsObject, JsString}
 
 import scala.util.Try
 
 class ProductService @Inject() (
-  productRepository: ProductRepository) {
+  productRepository: ProductRepository,
+  productTemplateRepository: ProductTemplateRepository                             ) {
   def toJsonList(product: Seq[Product]): Seq[JsObject] = {
-    product.map(row => toJson(row))
+    product.map(row => {toJson(row, productTemplateRepository.findProductTemplateByProductId(row.id).get)})
   }
 
-  def toJson(product: Product): JsObject = JsObject(Seq(
+  def toJson(product: Product, productTemplate: ProductTemplate): JsObject = JsObject(Seq(
     "id" -> JsString(product.id.toString()),
     "name" -> JsString(product.name),
     "image" -> JsString(product.image),
     "description" -> JsString(product.description),
-    "price" -> JsNumber(product.price)))
+    "price" -> JsNumber(product.price),
+    "width" -> JsString(productTemplate.width.toString),
+    "height" -> JsString(productTemplate.width.toString),
+    "url" -> JsString(productTemplate.url),
+    "right" -> JsString(productTemplate.right.toString),
+    "left" -> JsString(productTemplate.left.toString),
+    "top" -> JsString(productTemplate.top.toString),
+    "bottom" -> JsString(productTemplate.top.toString)
+  ))
 
   def findAll(): Try[Seq[Product]] = productRepository.findAll
 }
